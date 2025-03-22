c34b9e90ebf8edae3790b437596806d5ac6fc5be
Bombalaka <yotaka.johansson@hotmail.com>
2025-03-22 22:13:46 +0100

    refactor : add MongoDbOptions configuration class

---
+++ Configurations/MongoDbOptions.cs
@@ -0,0 +1,13 @@
+
+
+namespace BarbellaInventory.Configurations
+{
+    public class MongoDbOptions
+    {
+        public const string SectionName = "MongoDb";
+
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string BarbiesCollectionName { get; set; } = string.Empty;   
+    }
+}
