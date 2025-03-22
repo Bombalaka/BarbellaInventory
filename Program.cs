29e7d53243e6074a54a97fa90b4bef733d67f065
Bombalaka <yotaka.johansson@hotmail.com>
2025-03-22 22:46:58 +0100

    feat: update appsettings for MongoDB integration and enhance UI styles

--- Program.cs
+++ Program.cs
@@ -1,7 +1,55 @@
+using BarbellaInventory.Configurations;
+using BarbellaInventory.Models;
+using BarbellaInventory.Repositories;
+using BarbellaInventory.Services;
+using MongoDB.Driver;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Check if MongoDB should be used (default to false if not specified)
+bool useMongoDb = builder.Configuration.GetValue<bool>("FeatureFlags:UseMongoDb");
+
+if (useMongoDb)
+{
+    // Configure MongoDB options
+    builder.Services.Configure<MongoDbOptions>(
+        builder.Configuration.GetSection(MongoDbOptions.SectionName));
+
+    // Configure MongoDB client
+    builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+    {
+        var mongoDbOptions = builder.Configuration.GetSection(MongoDbOptions.SectionName).Get<MongoDbOptions>();
+        return new MongoClient(mongoDbOptions?.ConnectionString);
+    });
+
+    // Configure MongoDB collection
+    builder.Services.AddSingleton<IMongoCollection<BarbieSet>>(serviceProvider =>
+    {
+        var mongoDbOptions = builder.Configuration.GetSection(MongoDbOptions.SectionName).Get<MongoDbOptions>();
+        var mongoClient = serviceProvider.GetRequiredService<IMongoClient>();
+        var database = mongoClient.GetDatabase(mongoDbOptions?.DatabaseName);
+        return database.GetCollection<BarbieSet>(mongoDbOptions?.BarbiesCollectionName);
+    });
+
+    // Register MongoDB repository
+    builder.Services.AddSingleton<IBarbellaRepository, MongoDbRepository>();
+
+    Console.WriteLine("Using MongoDB repository");
+}
+else
+{
+    // Register in-memory repository as fallback
+    builder.Services.AddSingleton<IBarbellaRepository, InMemoryRepository>();
+
+    Console.WriteLine("Using in-memory repository");
+}
+
+// Register service (depends on repository)
+builder.Services.AddScoped<IBarbellaService, BarbellaService>();
 
 var app = builder.Build();
 
--- obj/Debug/net9.0/BarbellaInventory.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarbellaInventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarbellaInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e4d8150201210b1d2f2d23003cec915131ec70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b5c8bc3a0913f88716a17dbacd88d00b695178")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarbellaInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarbellaInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
